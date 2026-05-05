5b7c60665c3d9c7976cdbafcc22ace6c699075b2
jrporto2 <86452124+jrporto2@users.noreply.github.com>
2026-05-04 23:37:44 -0500

    Comment out chmod for create-client.sh
    Comment out the chmod command for create-client.sh.

--- datadrive/run.sh
+++ datadrive/run.sh
@@ -10,7 +10,7 @@ rm -rf $DESTINATION
 # Clone Odoo directory
 git clone --depth=1 https://github.com/jrporto2/odoo-18-docker-compose_saas.git $DESTINATION
 rm -rf $DESTINATION/.git
-chmod +x $DESTINATION/datadrive/admin/scripts/create-client.sh
+# chmod +x $DESTINATION/datadrive/admin/scripts/create-client.sh
 # Run Odoo
 docker compose -f $DESTINATION/docker-compose.yml up -d
 echo "Odoo started at http://localhost:$PORT | Master Password: $MASTERPASSWORD | Live chat port: $CHAT"
