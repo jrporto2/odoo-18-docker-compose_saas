e21526a67938336d87ab142e66d0a37032a66dbe
jrporto2 <86452124+jrporto2@users.noreply.github.com>
2026-05-08 00:54:00 -0500

    Update run.sh

--- datadrive/run.sh
+++ datadrive/run.sh
@@ -12,14 +12,14 @@ rm -rf $DESTINATION
 # Clone Odoo directory
 git clone --depth=1 https://github.com/jrporto2/odoo-18-docker-compose_saas.git $DESTINATION
 rm -rf $DESTINATION/.git
+sudo chown -R 5050:5050 $DESTINATION/datadrive/pgadmin
+sudo chmod -R 700 $DESTINATION/datadrive/pgadmin
 # chmod +x $DESTINATION/datadrive/admin/scripts/create-client.sh
 # Run Odoo
 docker compose -f $DESTINATION/datadrive/core/docker-compose.yml up -d
 echo "Odoo started at http://localhost:$PORT | Master Password: $MASTERPASSWORD | Live chat port: $CHAT"
 sed -i 's|DESTINATION|'$BASE/$DESTINATION'|g' $DESTINATION/datadrive/admin/scripts/create-client.sh 
 sudo chmod -R 750 $DESTINATION/datadrive/admin/scripts/create-client.sh
-sudo chown -R 5050:5050 $DESTINATION/datadrive/pgadmin
-sudo chmod -R 700 $DESTINATION/datadrive/pgadmin
 #cd ~/odoo-saas/datadrive/templates
 #sudo ln ~/odoo-saas/datadrive/core/.env .env
 
